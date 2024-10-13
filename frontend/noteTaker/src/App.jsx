a84bdaf5d2c76e722e8727981198ca3c6fd9100f
Shivraj <jaldeshivraj@gmail.com>
2024-10-13 16:12:59 +0530

    FEATURE / ADDED TAILWIND

--- frontend/noteTaker/src/App.jsx
+++ frontend/noteTaker/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import SignUp from './pages/SignUp/SignUp'
 
+const routes = <Router>
+  <Routes>
+    <Route exact path='/dashboard' element={<Home />} />
+    <Route exact path='/login' element={<Login />} />
+    <Route exact path='/SignUp' element={<SignUp />} />
+  </Routes>
+</Router>
 const App = () => {
   return (
-    <div>App</div>
+    <div>{routes}</div>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./noteTaker/src/index.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
