94435a3e573d7c8013735578bedddde05b04dd4f
Shivraj <jaldeshivraj@gmail.com>
2024-10-17 10:08:35 +0530

    ADD VALIDATION TO LOGIN PAGE

--- Client/NoteTaker/src/pages/Login/Login.jsx
+++ Client/NoteTaker/src/pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { FaEyeSlash } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom'
 import { Box, Button, IconButton, InputAdornment, TextField, Typography } from '@mui/material'
+import { validateEmail, validatePassword } from '../../utils/helper.mjs';
 
 const Login = () => {
   const navigate = useNavigate()
@@ -11,9 +12,15 @@ const Login = () => {
     email: '',
     password: ''
   })
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
+
   const [showPassword, setShowPassword] = useState(false)
 
 
+  const handleLogin = () => {
+    navigate('/dashboard')
+  }
   const toggleShowPassword = () => {
     setShowPassword(!showPassword)
   }
@@ -39,87 +46,115 @@ const Login = () => {
           py: 7,
           boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.15)',
         }}>
-          <form>
-            <Typography sx={{ fontSize: '35px', mb: 2 }}>
-              Login
-            </Typography>
+          <Typography sx={{ fontSize: '35px', mb: 2 }}>
+            Login
+          </Typography>
 
 
-            <TextField label='Email *' type='text' className='input-box' sx={{
+          <TextField label='Email *'
+            onChange={(e) => {
+              let inputValue = e.target.value;
+              if (!validateEmail(inputValue)) {
+                setIsEmailError(true)
+              }
+              setLoginData({
+                ...loginData,
+                email: inputValue
+              })
+
+              if (inputValue === '') {
+                setIsEmailError(false)
+              }
+            }}
+            type='text'
+            className='input-box'
+            sx={{
               width: '100%',
               background: 'transparant',
-
               mb: 2
-            }} />
+            }}
+            error={isEmailError && loginData?.email === '' || isEmailError && !validateEmail(loginData?.email)}
+            helperText={isEmailError &&
+              loginData?.email === '' ? 'Email is required' : isEmailError && !validateEmail(loginData.email) ? 'Email is invalid' : ''
+            }
+          />
 
-            <TextField
-              label='Password *'
+          <TextField
+            label='Password *'
+            sx={{
+              width: "100%",
+            }}
+            type={showPassword ? 'text' : 'password'}
+            onChange={(e) => {
+              const inputValue = e.target.value
+              if (!validatePassword(inputValue)) {
+                setIsPasswordError(true)
+              }
+              setLoginData({
+                ...loginData,
+                password: inputValue
+              })
+            }}
+            value={loginData.password}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={toggleShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <IoEyeSharp />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={isPasswordError && loginData.password === '' || isPasswordError && !validatePassword(loginData.password)}
+            helperText={
+              isPasswordError && loginData.password === '' ? 'Password is Required' : isPasswordError && !validatePassword(loginData.password) ? 'Invalid password: Must include at least 8 char, an 1 num, an 1 uppercase letter, an 1 special char' : ''
+            }
+          />
+          <Typography sx={{
+            textAlign: 'end'
+            , mb: 3
+          }}>
+            <Button
+              disableRipple
               sx={{
-                width: "100%",
-                // mb: 2
-              }}
-              type={showPassword ? 'text' : 'password'}
-              onChange={(e) => {
-                setLoginData(e.target.value)
-              }}
-              value={loginData.password}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={toggleShowPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <FaEyeSlash /> : <IoEyeSharp />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <Typography sx={{
-              textAlign: 'end'
-              , mb: 3
-            }}>
-              <Button
-                disableRipple
-                sx={{
-                  textTransform: "capitalize",
-                  background: 'transparant',
-                  '&:hover': {
-                    backgroundColor: 'transparent',  // Add hover styles here
-                  }
-                }}>Forgot Password</Button>
-            </Typography>
-            <Button onClick={() => { }} sx={{
-              background: 'lightblue',
-              width: '100%',
-              py: 1,
-              mb: 2
-            }}>
-              Login
-            </Button>
+                textTransform: "capitalize",
+                background: 'transparant',
+                '&:hover': {
+                  backgroundColor: 'transparent',  // Add hover styles here
+                }
+              }}>Forgot Password</Button>
+          </Typography>
+          <Button onClick={handleLogin} sx={{
+            background: 'lightblue',
+            width: '100%',
+            py: 1,
+            mb: 2
+          }}>
+            Login
+          </Button>
 
-            <Box sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              gap: 1
-            }}>
-              <Typography>
-                {`Don't have an account ?`}
-              </Typography>
-              {/* <Link to='/signup' className='' style={{
-              }}>Register here</Link> */}
-              <Typography sx={{
-                cursor: 'pointer',
-                textDecoration: 'underline',
-                color: 'blue'
-              }} onClick={() => {
-                navigate('/signup')
-              }}>Register Here</Typography>
-            </Box>
-          </form>
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 1
+          }}>
+            <Typography>
+              {`Don't have an account ?`}
+            </Typography>
+
+            <Typography sx={{
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              color: 'blue'
+            }} onClick={() => {
+              navigate('/signup')
+            }}>Register Here</Typography>
+          </Box>
 
         </Box>
       </Box>
